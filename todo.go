d823fc7a6cec8b0ed1de7b8ebbbe8c997ab8334a
Böckler, Kevin <Kevin.Boeckler@mach.de>
2023-07-27 07:36:12 +0200

    Remove complete func
    since not needed

--- todo.go
+++ todo.go
@@ -313,37 +313,6 @@ type notification struct {
 	NotifiedAt time.Time        `yaml:"notifiedAt,omitempty"`
 }
 
-// complete performs bash command line completion for defined flags
-// see https://blog.jbowen.dev/2019/11/bash-command-line-completion-with-go/
-// Bootstrap completion on terminal by complete -C `pwd`/todo.exe ./todo.exe
-func complete() {
-
-	// when Bash calls the command to perform completion it will
-	// set several environment variables including COMP_LINE.
-	// If this variable is not set, then command is being invoked
-	// normally and we can return.
-	if _, ok := os.LookupEnv("COMP_LINE"); !ok {
-		return
-	}
-
-	// Get the current partial word to be completed
-	partial := os.Args[2]
-
-	// strip leading '-' from partial, if present
-	partial = strings.TrimLeft(partial, "-")
-
-	// Loop through all defined flags and find any that start
-	// with partial (or return all flags if partial is empty string)
-	// Matching words are returned to Bash via stdout
-	flag.VisitAll(func(f *flag.Flag) {
-		if partial == "" || strings.HasPrefix(f.Name, partial) {
-			fmt.Println("-" + f.Name)
-		}
-	})
-
-	os.Exit(0)
-}
-
 type server struct {
 	app    *todoApp
 	ctx    context.Context
