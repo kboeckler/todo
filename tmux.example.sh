3bebc7136cee8f59fba6ae244d583ac66c6a7594
Böckler, Kevin <Kevin.Boeckler@mach.de>
2024-02-02 14:51:28 +0100

    Add working tmux example

--- tmux.example.sh
+++ tmux.example.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+# Usage: inside your ~/.tmux.conf set:
+# set -g status-right '#[fg=colour0,bg=colour254 bold] 🔔 #( /path/to/todo/todo.tmux.sh ) | 🗓️   %d/%m #[fg=colour0,bg=colour254 bold]| ⏲️  %H:%M:%S '
 
-while :
-do
-	./todo due | wc -l
-	sleep 1
-done
+DUE=$(todo due | wc -l)
+ALL=$(todo list | wc -l)
+echo "$ALL($DUE)"
 
